3994363a8cc2695a8d7fd33cee6d9af59e03b6da
alan-lira <alan_lira@id.uff.br>
2021-08-01 11:55:14 -0300

    Coded generate_dataframes_list function.

--- diffsequences_differentiator/differentiator.py
+++ diffsequences_differentiator/differentiator.py
@@ -4,6 +4,7 @@ from logging import basicConfig, getLogger, INFO, Logger
 from pathlib import Path
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
+from pyspark.sql.types import LongType, StringType, StructType
 import ast
 import os
 import sys
@@ -27,6 +28,7 @@ class DiffSequencesParameters:
 
     def __init__(self):
         self.sequences_path_list_text_file = None
+        self.diff_approach = None
 
 
 def check_if_is_valid_number_of_arguments(number_of_arguments_provided: int) -> None:
@@ -62,6 +64,9 @@ def load_diff_sequences_parameters(dsp: DiffSequencesParameters,
     # READ FASTA SEQUENCES PATH LIST TEXT FILE
     dsp.sequences_path_list_text_file = \
         Path(str(parsed_parameters_dictionary["DiffSequences"]["sequences_path_list_text_file"]))
+
+    # READ DIFF APPROACH
+    dsp.diff_approach = int(parsed_parameters_dictionary["DiffSequences"]["diff_approach"])
 
 
 def get_or_create_spark_session() -> SparkSession:
@@ -120,6 +125,14 @@ def get_spark_executor_memory(spark_context: SparkContext) -> str:
         return "1G"
     else:
         return spark_context.getConf().get("spark.executor.memory")
+
+
+def get_spark_max_partition_size_in_bytes(spark_context: SparkContext) -> int:
+    is_running_locally_non_cluster = check_if_is_running_locally_non_cluster(spark_context)
+    if is_running_locally_non_cluster:
+        return 134217728  # (128 MB)
+    else:
+        return int(spark_context.getConf().get("spark.sql.files.maxPartitionBytes"))
 
 
 def start_diff_sequences_spark(dss: DiffSequencesSpark,
@@ -214,7 +227,7 @@ def generate_sequences_list(sequences_path_list_text_file: Path,
                             logger: Logger):
     # GENERATE SEQUENCES LIST
     read_sequences_start = time.time()
-    sequences_list = parse_sequences_list(sequences_path_list_text_file)
+    parsed_sequences_list = parse_sequences_list(sequences_path_list_text_file)
     read_sequences_end = time.time()
     read_sequences_seconds = read_sequences_end - read_sequences_start
     read_sequences_minutes = read_sequences_seconds / 60
@@ -223,7 +236,204 @@ def generate_sequences_list(sequences_path_list_text_file: Path,
                 str(round(read_sequences_seconds, 4)),
                 str(round(read_sequences_minutes, 4)))
     logger.info(generate_sequences_list_duration_message)
-    return sequences_list
+    return parsed_sequences_list
+
+
+def estimate_dataframe_size_in_bytes(sequence_length: int,
+                                     dataframe_schema: StructType) -> int:
+    longtype_count = 0
+    longtype_default_size = 8  # LongType(): 8 Bytes
+    stringtype_count = 0
+    stringtype_default_size = 4  # StringType(): 4 Bytes + (String Length * 1 Byte)
+    dataframe_schema_datatypes_list = [field.dataType for field in dataframe_schema.fields]
+    for datatype in dataframe_schema_datatypes_list:
+        if datatype == LongType():
+            longtype_count = longtype_count + 1
+        elif datatype == StringType():
+            stringtype_count = stringtype_count + 1
+    longtype_size_one_row = longtype_count * longtype_default_size
+    stringtype_size_one_row = stringtype_count * (stringtype_default_size + 1)
+    estimated_dataframe_size_in_bytes = sequence_length * (longtype_size_one_row + stringtype_size_one_row)
+    return estimated_dataframe_size_in_bytes
+
+
+def calculate_number_of_dataframe_partitions(spark_context: SparkContext,
+                                             dataframe_size_in_bytes: int) -> int:
+    max_partition_size = get_spark_max_partition_size_in_bytes(spark_context)
+    total_cores_count = get_spark_cores_max_count(spark_context)
+    divider = total_cores_count
+    while True:
+        partitions_size = dataframe_size_in_bytes / divider
+        if partitions_size < max_partition_size:
+            number_of_dataframe_partitions = divider
+            break
+        divider = divider + 1
+    return number_of_dataframe_partitions
+
+
+# TODO: REFACTOR
+def generate_dataframes_list_from_sequences_list(spark_session: SparkSession,
+                                                 spark_context: SparkContext,
+                                                 sequences_list: list) -> list:
+    dataframes_list = []
+    dataframe_index = 0
+    for sequence_index in range(len(sequences_list)):
+        dataframe_data = []
+        index_label = "Index"
+        sequence_identification = sequences_list[sequence_index][0]
+        if sequence_identification != "Seq":
+            char_label = "Seq_" + sequence_identification
+        else:
+            char_label = "Seq_" + str(dataframe_index)
+        sequence_data = sequences_list[sequence_index][1]
+        sequence_length = len(sequence_data)
+        for index in range(sequence_length):
+            dataframe_data.append((index, sequence_data[index]))
+        dataframe_schema = StructType() \
+            .add(index_label, LongType(), nullable=False) \
+            .add(char_label, StringType(), nullable=False)
+        estimated_dataframe_size_in_bytes = estimate_dataframe_size_in_bytes(sequence_length,
+                                                                             dataframe_schema)
+        dataframe = spark_session.createDataFrame(data=dataframe_data,
+                                                  schema=dataframe_schema,
+                                                  verifySchema=True)
+        number_of_dataframe_partitions = calculate_number_of_dataframe_partitions(spark_context,
+                                                                                  estimated_dataframe_size_in_bytes)
+        dataframe = dataframe.coalesce(number_of_dataframe_partitions)
+        dataframes_list.append((dataframe, sequence_length))
+        dataframe_index = dataframe_index + 1
+    return dataframes_list
+
+
+def generate_sequences_indices_blocks_list(sequences_list_length: int) -> list:
+    sequences_indices_blocks_list = []
+    first_block_sequences_indices_list = []
+    second_block_sequences_indices_list = []
+    for first_block_sequence_index in range(0, sequences_list_length - 1):
+        first_block_sequences_indices_list.append(first_block_sequence_index)
+        for second_block_sequence_index in range(first_block_sequence_index + 1, sequences_list_length):
+            second_block_sequences_indices_list.extend([second_block_sequence_index])
+        if len(first_block_sequences_indices_list) > 0 and len(second_block_sequences_indices_list) > 0:
+            sequences_indices_blocks_list.append([first_block_sequences_indices_list,
+                                                  second_block_sequences_indices_list])
+        first_block_sequences_indices_list = []
+        second_block_sequences_indices_list = []
+    return sequences_indices_blocks_list
+
+
+def get_biggest_sequence_length_from_blocks(sequences_list: list,
+                                            first_block_sequences_indices_list: list,
+                                            second_block_sequences_indices_list: list) -> int:
+    biggest_sequence_length_from_blocks = 0
+    first_block_sequence_length = len(sequences_list[first_block_sequences_indices_list[0]][1])
+    if biggest_sequence_length_from_blocks < first_block_sequence_length:
+        biggest_sequence_length_from_blocks = first_block_sequence_length
+    for index_second_block_sequences_indices_list in range(len(second_block_sequences_indices_list)):
+        second_block_index_sequence_length = \
+            len(sequences_list[second_block_sequences_indices_list[index_second_block_sequences_indices_list]][1])
+        if biggest_sequence_length_from_blocks < second_block_index_sequence_length:
+            biggest_sequence_length_from_blocks = second_block_index_sequence_length
+    return biggest_sequence_length_from_blocks
+
+
+# TODO: REFACTOR
+def generate_dataframes_list_from_sequences_index_blocks_list(spark_session: SparkSession,
+                                                              spark_context: SparkContext,
+                                                              sequences_list: list) -> list:
+    dataframes_list = []
+    sequences_indices_blocks_list = generate_sequences_indices_blocks_list(len(sequences_list))
+    for index_sequences_indices_blocks_list in range(len(sequences_indices_blocks_list)):
+        first_block_sequences_indices_list = sequences_indices_blocks_list[index_sequences_indices_blocks_list][0]
+        second_block_sequences_indices_list = sequences_indices_blocks_list[index_sequences_indices_blocks_list][1]
+        biggest_sequence_length_from_blocks = \
+            get_biggest_sequence_length_from_blocks(sequences_list,
+                                                    first_block_sequences_indices_list,
+                                                    second_block_sequences_indices_list)
+        first_dataframe_data = []
+        first_dataframe_index_label = "Index"
+        first_block_sequence_identification = sequences_list[first_block_sequences_indices_list[0]][0]
+        if first_block_sequence_identification != "Seq":
+            first_dataframe_char_label = "Seq_" + first_block_sequence_identification
+        else:
+            first_dataframe_char_label = "Seq_" + "0"
+        first_dataframe_schema = StructType() \
+            .add(first_dataframe_index_label, LongType(), nullable=False) \
+            .add(first_dataframe_char_label, StringType(), nullable=True)
+        second_dataframe_data = []
+        second_dataframe_data_aux = []
+        second_dataframe_index_label = "Index"
+        second_dataframe_schema = StructType() \
+            .add(second_dataframe_index_label, LongType(), nullable=False)
+        for index_second_block_sequences_indices_list in range(len(second_block_sequences_indices_list)):
+            second_block_sequence_identification = \
+                sequences_list[second_block_sequences_indices_list[index_second_block_sequences_indices_list]][0]
+            if second_block_sequence_identification != "Seq":
+                second_dataframe_char_label = "Seq_" + second_block_sequence_identification
+            else:
+                second_dataframe_char_label = "Seq_" + str(index_second_block_sequences_indices_list + 1)
+            second_dataframe_schema.add(second_dataframe_char_label, StringType(), nullable=True)
+        for index_biggest_sequence_length_from_blocks in range(biggest_sequence_length_from_blocks):
+            try:
+                if sequences_list[first_block_sequences_indices_list[0]][1][index_biggest_sequence_length_from_blocks]:
+                    first_dataframe_data.append((index_biggest_sequence_length_from_blocks, sequences_list[first_block_sequences_indices_list[0]][1][index_biggest_sequence_length_from_blocks]))
+            except IndexError:
+                first_dataframe_data.append((index_biggest_sequence_length_from_blocks, None))
+            second_dataframe_data_aux.append(index_biggest_sequence_length_from_blocks)
+            for index_second_block_sequences_indices_list in range(len(second_block_sequences_indices_list)):
+                try:
+                    if sequences_list[second_block_sequences_indices_list[index_second_block_sequences_indices_list]][1][index_biggest_sequence_length_from_blocks]:
+                        second_dataframe_data_aux.append((sequences_list[second_block_sequences_indices_list[index_second_block_sequences_indices_list]][1][index_biggest_sequence_length_from_blocks]))
+                except IndexError:
+                    second_dataframe_data_aux.append(None)
+            second_dataframe_data.append(second_dataframe_data_aux)
+            second_dataframe_data_aux = []
+        estimated_first_dataframe_size_in_bytes = \
+            estimate_dataframe_size_in_bytes(biggest_sequence_length_from_blocks, first_dataframe_schema)
+        first_dataframe = spark_session.createDataFrame(data=first_dataframe_data,
+                                                        schema=first_dataframe_schema,
+                                                        verifySchema=True)
+        first_dataframe_number_of_partitions = \
+            calculate_number_of_dataframe_partitions(spark_context, estimated_first_dataframe_size_in_bytes)
+        first_dataframe = first_dataframe.coalesce(first_dataframe_number_of_partitions)
+        dataframes_list.append((first_dataframe, biggest_sequence_length_from_blocks))
+        estimated_second_dataframe_size_in_bytes = \
+            estimate_dataframe_size_in_bytes(biggest_sequence_length_from_blocks, second_dataframe_schema)
+        second_dataframe = spark_session.createDataFrame(data=second_dataframe_data,
+                                                         schema=second_dataframe_schema,
+                                                         verifySchema=True)
+        second_dataframe_number_of_partitions = \
+            calculate_number_of_dataframe_partitions(spark_context, estimated_second_dataframe_size_in_bytes)
+        second_dataframe = second_dataframe.coalesce(second_dataframe_number_of_partitions)
+        dataframes_list.append((second_dataframe, biggest_sequence_length_from_blocks))
+    return dataframes_list
+
+
+def generate_dataframes_list(spark_session: SparkSession,
+                             spark_context: SparkContext,
+                             app_name: str,
+                             diff_approach: int,
+                             sequences_list: list,
+                             logger: Logger) -> list:
+    # GENERATE DATAFRAMES LIST
+    generate_dataframes_start = time.time()
+    dataframes_list = []
+    if diff_approach == 1:
+        dataframes_list = generate_dataframes_list_from_sequences_list(spark_session,
+                                                                       spark_context,
+                                                                       sequences_list)
+    elif diff_approach == 2:
+        dataframes_list = generate_dataframes_list_from_sequences_index_blocks_list(spark_session,
+                                                                                    spark_context,
+                                                                                    sequences_list)
+    generate_dataframes_end = time.time()
+    generate_dataframes_seconds = generate_dataframes_end - generate_dataframes_start
+    generate_dataframes_minutes = generate_dataframes_seconds / 60
+    generate_dataframes_list_message = "({0}) Generate Dataframes List Duration: {1} sec (≈ {2} min)" \
+        .format(app_name,
+                str(round(generate_dataframes_seconds, 4)),
+                str(round(generate_dataframes_minutes, 4)))
+    logger.info(generate_dataframes_list_message)
+    return dataframes_list
 
 
 def stop_diff_sequences_spark(dss: DiffSequencesSpark,
@@ -294,6 +504,13 @@ def diff(argv: list) -> None:
                                              dss.app_name,
                                              logger)
 
+    # GENERATE DATAFRAMES LIST
+    dataframes_list = generate_dataframes_list(dss.spark_session,
+                                               dss.spark_context,
+                                               dss.app_name,
+                                               dsp.diff_approach,
+                                               sequences_list,
+                                               logger)
     # STOP DIFF SEQUENCES SPARK
     stop_diff_sequences_spark(dss, logger)
 
